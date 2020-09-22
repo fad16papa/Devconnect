996d99d9eca237ffa4c8cf3db70680213f4ff666
Francis Decena <francisdecena@Franciss-MacBook-Pro.local>
2020-09-22 16:29:56 +0800

    Commit Changes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -13,7 +13,6 @@ const { check, validationResult } = require("express-validator");
 router.get("/", auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
-    console.log(user);
 
     res.json(user);
   } catch (error) {
